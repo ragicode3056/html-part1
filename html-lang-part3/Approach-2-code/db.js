5d0b08be89f0ff4476cf1baff11c655201e59877
ragicode3056 <raginijayakumar96@gmail.com>
2022-03-08 16:26:15 +0530

    html-languages-added

---
+++ html-lang-part3/Approach-2-code/db.js
@@ -0,0 +1,20 @@
+var data = {
+			  "english": 
+			  {
+			    "title": "OFS Learning",
+			    "description": 
+				    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non adipisci eligendi repellat ad dolor veritatis itaque sequi minus iste, doloremque. Officiis non eaque atque excepturi repudiandae nulla eos eligendi magni molestiae eius distinctio, voluptas pariatur incidunt et culpa inventore aspernatur recusandae nihil asperiores, vitae, maiores laborum quasi perspiciatis natus dignissimos! Accusantium aliquam nostrum impedit dignissimos iste, iure inventore! Sapiente, labore earum ut dicta ducimus asperiores laudantium natus officiis, quisquam placeat aspernatur voluptatum aut voluptates tenetur quos magni fugit quia. Fugiat."
+			  },
+			  "tamil": 
+			  {
+			    "title": "கற்றல்",
+			    "description": 
+				    "வலியே வலி அதிகம் வலியை அடைய வேண்டாம் என்பதைத் தேர்ந்தெடுப்பது உண்மைக்குத் தள்ளுகிறது, எனவே இதைப் பின்பற்றுவதற்கான வாய்ப்புகள் குறைவு, மேலும் வலி. அவர்கள் இந்தக் கடமைகளை ஏற்கக்கூடாது, அவற்றைத் தேர்ந்தெடுப்பதில் பெரும் எரிச்சல் என்ற வேறுபாடு இல்லாமல் நிராகரிக்கப்பட வேண்டும், இன்பம் என்பது விளைவான வீழ்ச்சி, மற்றும் கண்டுபிடிப்பாளரின் தவறு, நீங்கள் பிறந்ததைப் போல கடுமையான, வாழ்க்கை மற்றும் பெரிய உழைப்பு எதையும் ஏற்காததால் அவர்களை நிராகரிக்கிறது. சந்திக்க தகுதியானவர்! இது எங்கள் மிகவும் தகுதியான குற்றம் சாட்டுபவர்களில் சிலரைத் தடுக்கிறது. முதியவரின் முட்கள் நிறைந்த கடமைகளைப் போற்றுவதில் குறிப்பிடப்பட்டவர்களின் கடின உழைப்பை நாம் எண்ணும்போது, ​​ஞானமுள்ள மனிதன், அவர் முக்கியத்துவம் வாய்ந்தவர் என்பதால், அவர் தவிர்க்க வேண்டிய இன்பங்கள் அல்லது இன்பங்களில் எந்த இன்பத்தையும் நிராகரிக்கிறார். அவன் ஓடிப்போகட்டும்."
+			  },
+			  "japanese": 
+			  {
+			    "title": "学習の",
+			    "description": 
+				    "ﾂつｨﾂ知ﾂづｧﾂつｹﾂ-ﾂ新ﾂ陳ﾂ湘ｮﾂ陛ｱ これらの苦痛から真実の苦痛に選ぶために得てはいけない、従ってより少しにこの苦痛に従ってはいけない。 サービスではないと、彼の区別の大きな不快感から選択するそれらの否認を歓迎しない、結果として生じた事件の喜びと障害エクスプローラが受け入 このことについて私たちの下駄のいくつかを非難,右エクスプローラ! 的に仕事をしていまasperioresリストの生まれのサービスをつけてください編集喜びや楽しみの開催なども走りました。 走れ。."
+			  }
+			}
